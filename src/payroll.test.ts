f97769c19521eafe59de289e7610ad1553609e12
Jamie-Poeffel <Jamie.poeffel@gmail.com>
2025-05-14 13:55:21 +0200

    tests 2, 3 für aufgabe 2 und funktion calculatePayslip erstellt und getestet

--- src/payroll.test.ts
+++ src/payroll.test.ts
@@ -6,17 +6,53 @@ describe("Payroll Tests", () => {
         const gehalt: Salary = {
             born: new Date("10.3.2008"),
             payday: new Date("25.12.2025"),
-            gross: 8400,
+            gross: 700,
         }
 
         const result: Payslip = {
             salary: gehalt,
-            deductions: new Map(),
-            totalDeductions: 2500,
-            net: 5900,
+            deductions: new Map([["ALV und NBU", 153.72]]),
+            totalDeductions: 153.72,
+            net: 687.19,
         }
 
-        expect(calculatePayslip(gehalt)).toBe(result)
+        expect(calculatePayslip(gehalt)).toMatchObject(result)
     })
 
+    // ein 18 jähriger Lernender mit einem Monatsgehalt von 1200.-
+
+    it("ein 18 jähriger Lernender mit einem Monatsgehalt von 1200.-", () => {
+        const gehalt: Salary = {
+            born: new Date("10.3.2006"),
+            payday: new Date("25.12.2025"),
+            gross: 1200,
+        }
+
+        const result: Payslip = {
+            salary: gehalt,
+            deductions: new Map([["ALV und NBU", 263.52], ["AHV, IV und EO", 1526.3999999999999]]),
+            totalDeductions: 1789.9199999999998,
+            net: 1050.84,
+        }
+
+        expect(calculatePayslip(gehalt)).toMatchObject(result)
+    })
+
+    // ein 21 jähriger Angestellter mit einem Monatsgehalt von 5900.-
+    it("ein 21 jähriger Angestellter mit einem Monatsgehalt von 5900.-", () => {
+        const gehalt: Salary = {
+            born: new Date("10.3.2004"),
+            payday: new Date("25.12.2025"),
+            gross: 5900,
+        }
+
+        const result: Payslip = {
+            salary: gehalt,
+            deductions: new Map([["ALV und NBU", 1295.64], ["AHV, IV und EO", 7504.799999999999], ["PK", 6301.2]]),
+            totalDeductions: 15101.64,
+            net: 4641.53,
+        }
+
+        expect(calculatePayslip(gehalt)).toMatchObject(result)
+    })
 })
--- src/payroll.ts
+++ src/payroll.ts
@@ -23,13 +23,47 @@ export type Payslip = {
 };
 
 export function calculatePayslip(salary: Salary): Payslip {
-  // TODO: implement
   const result: Payslip = {
     salary: salary,
     deductions: new Map(),
     totalDeductions: 0.0,
     net: salary.gross,
   };
+
+  // TODO: implement
+  if (salary.born.getFullYear() < 2008) {
+    result.deductions.set(
+      "AHV, IV und EO",
+      ((salary.gross * 12 / 100 * DEDUCTION_RATES.get("AHV")) +
+        (salary.gross * 12 / 100 * DEDUCTION_RATES.get("IV")) +
+        (salary.gross * 12 / 100 * DEDUCTION_RATES.get("EO")))
+    )
+  }
+
+  if (salary.gross * 12 >= 2_500) {
+    result.deductions.set(
+      "ALV und NBU",
+      ((salary.gross * 12 / 100 * DEDUCTION_RATES.get("ALV")) +
+        (salary.gross * 12 / 100 * DEDUCTION_RATES.get("NBU")))
+    )
+  }
+
+  if (salary.gross * 12 >= 22_680) {
+    result.deductions.set(
+      "PK",
+      (salary.gross * 12 / 100 * DEDUCTION_RATES.get("PK"))
+    )
+  }
+
+  let val: number = 0
+
+  val += result.deductions.has("AHV, IV und EO") ? result.deductions.get("AHV, IV und EO") : 0;
+  val += result.deductions.has("ALV und NBU") ? result.deductions.get("ALV und NBU") : 0;
+  val += result.deductions.has("PK") ? result.deductions.get("PK") : 0;
+
+  result.totalDeductions = val;
+  result.net = (salary.gross * 12 - result.totalDeductions) / 12
+
   return result;
 }
 
